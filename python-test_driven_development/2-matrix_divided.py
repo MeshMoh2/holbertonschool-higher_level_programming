901512458c543dd8882ec867f4ad7c8379f8a614
Your Name <you@example.com>
2025-05-18 11:11:54 +0000

    T

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -25,13 +25,21 @@ def matrix_divided(matrix=None, div=None):
         ZeroDivisionError: If div is 0.
     """
     if matrix is None or div is None:
-        raise TypeError("matrix_divided() missing required arguments 'matrix' and/or 'div'")
+        raise TypeError(
+            "matrix_divided() missing required arguments 'matrix' and/or 'div'"
+        )
 
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(matrix, list) or not all(
+        isinstance(row, list) for row in matrix
+    ):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
 
     if matrix == [] or any(row == [] for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
 
     row_length = len(matrix[0])
     for row in matrix:
@@ -39,13 +47,18 @@ def matrix_divided(matrix=None, div=None):
             raise TypeError("Each row of the matrix must have the same size")
         for element in row:
             if not isinstance(element, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) of integers/floats"
+                )
 
-    if (not isinstance(div, (int, float))
-            or div != div
-            or div == float('inf')
-            or div == float('-inf')):
+    if (
+        not isinstance(div, (int, float))
+        or div != div
+        or div == float('inf')
+        or div == float('-inf')
+    ):
         raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
 

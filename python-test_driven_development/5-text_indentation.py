1c770817abcb090beb69b5ba7c5227cd816d0cd9
Your Name <you@example.com>
2025-05-18 11:56:50 +0000

    T

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -18,13 +18,12 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    i = 0
-    while i < len(text):
-        print(text[i], end="")
-        if text[i] in ".:?":
-            print("\n")
-            i += 1
-            while i < len(text) and text[i] == " ":
-                i += 1
-            continue
-        i += 1
+    buffer = ""
+    for char in text:
+        buffer += char
+        if char in ".:?":
+            print(buffer.strip())
+            print()
+            buffer = ""
+    if buffer.strip():
+        print(buffer.strip())

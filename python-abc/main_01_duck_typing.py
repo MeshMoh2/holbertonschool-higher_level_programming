38d915341d63d448b86087f32d335fbc7ac0ecce
Your Name <you@example.com>
2025-06-01 14:58:16 +0000

    T

--- python-abc/main_01_duck_typing.py
+++ python-abc/main_01_duck_typing.py
@@ -1,8 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""
+This script demonstrates polymorphism with abstract shapes
+by creating instances of Circle and Rectangle and using shape_info.
+"""
+
 from task_01_duck_typing import Circle, Rectangle, shape_info
 
-circle = Circle(radius=5)
-rectangle = Rectangle(width=4, height=7)
+if __name__ == "__main__":
+    circle = Circle(radius=5)
+    rectangle = Rectangle(width=4, height=7)
 
-shape_info(circle)
-shape_info(rectangle)
+    shape_info(circle)
+    shape_info(rectangle)

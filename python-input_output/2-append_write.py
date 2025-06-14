3df21f69bf4b9a94cd60f1415ea239b8597781df
Your Name <you@example.com>
2025-06-14 14:11:17 +0000

    T

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-"""a function that's convert any python ogject to JSON string"""
-import json
+"""a function that let the user choose a text and a filepath to write"""
 
 
-def to_json_string(my_obj):
-    """a function that returns the string equivelant to a python object"""
-    return json.dumps(my_obj)
+def append_write(filename="", text=""):
+    """append a UTF-8 text file and overwrite its content.
+
+    args: filename is the filepath, and text represent the string to write
+
+    returns the number of character to be written
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        txt = text
+        return f.write(txt)

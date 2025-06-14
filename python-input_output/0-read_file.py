0ce88d62ca982bf03001d48a71995dadd84738aa
Your Name <you@example.com>
2025-06-14 14:03:24 +0000

    T

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,11 +1,4 @@
-#!/usr/bin/python3
-"""Module that defines a function to read and print a file content."""
-
-
-
-def read_file(filename=""):
-    """Reads a UTF-8 text file and prints its content to stdout."""
-    with open(filename, "r", encoding="utf-8") as f:
-        content = f.read()
-        print(content, end="")
-        
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) and returns the number of characters written"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)

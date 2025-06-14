2ffaede58de721ae1d92194e88b532321442cf96
Your Name <you@example.com>
2025-06-14 14:38:51 +0000

    T

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -4,20 +4,19 @@
 
 def pascal_triangle(n):
     """Return Pascal triangle as a list of lists of integers."""
-        if n <= 0:
+    if n <= 0:
         return []
 
-    triangle = [[1]]  # First row
+    triangle = [[1]]
 
     for i in range(1, n):
         prev_row = triangle[-1]
-        new_row = [1]  # Start with 1
+        new_row = [1]
 
-        # Compute the middle values
         for j in range(1, len(prev_row)):
             new_row.append(prev_row[j - 1] + prev_row[j])
 
-        new_row.append(1)  # End with 1
+        new_row.append(1)
         triangle.append(new_row)
 
     return triangle

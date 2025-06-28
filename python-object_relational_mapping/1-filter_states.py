4dee48dd7583e7be57a07a635fca112e1d8ca08c
Your Name <you@example.com>
2025-06-28 19:57:11 +0000

    T

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-""" Lists all states starting with 'N' from the database hbtn_0e_0_usa """
+"""Lists all states starting with 'N' from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Get arguments
     username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    # Connect to database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -18,11 +16,8 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
 
-    # Execute query
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
-    # Print results
     for row in cursor.fetchall():
         print(row)
 

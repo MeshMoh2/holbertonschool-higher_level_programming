0cd774e84764b2cb0d51747793b8b9f5fdcb3a8b
Your Name <you@example.com>
2025-06-28 19:53:39 +0000

    T

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
+""" Lists all states from the database hbtn_0e_0_usa """
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Get MySQL credentials and database name from command line arguments
     username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    # Connect to the MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=password, db=db_name)
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=db_name)
 
-    # Create a cursor object to interact with the database
     cursor = db.cursor()
-
-    # Execute the SELECT query
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Fetch all rows and print them
     for row in cursor.fetchall():
         print(row)
 
-    # Clean up
     cursor.close()
     db.close()

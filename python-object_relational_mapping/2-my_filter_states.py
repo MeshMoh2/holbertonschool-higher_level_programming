a70e65e6152e063529fb684bee883d6b13d96585
Your Name <you@example.com>
2025-06-29 19:18:36 +0000

    T

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,36 +1,24 @@
 #!/usr/bin/python3
-"""Lists all states from the database where name matches the user input"""
+"""
+   whose name corresponds to the argument passed
+"""
 
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    # Extract arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Connect to MySQL database
     db = MySQLdb.connect(
         host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
-    )
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
 
-    cursor = db.cursor()
-
-    # Use string formatting (insecure on purpose for this task)
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
-    cursor.execute(query)
-
-    # Print each row exactly as returned
-    rows = cursor.fetchall()
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states
+                 WHERE BINARY name LIKE '{}%'
+                 ORDER BY id;""".format(argv[4]))
+    rows = cur.fetchall()
     for row in rows:
         print(row)
-
-    # Cleanup
-    cursor.close()
+    cur.close()
     db.close()
